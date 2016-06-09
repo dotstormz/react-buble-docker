0ab2492d7f39066c47e5104f422d267907621da7
Josh Girvin <josh@studionone.com.au>
2016-06-09 17:55:17 +1000

    Changes

--- code/client/app.js
+++ code/client/app.js
@@ -3,13 +3,32 @@
  */
 'use strict'
 
+// Dependencies
 const React = require('react')
-const render = require('react-dom').render
+const {render} = require('react-dom')
+const {PropTypes} = React
 
-const Main = ({ name }) => {
+const Main = ({name}) => {
   return <div>
     <h1>Hello, {name}!</h1>
+    <MyComponent />
   </div>
 }
 
-render(<Main name='Josh' />, document.getElementById('app'))
+Main.propTypes = {
+  name: PropTypes.string.isRequired
+}
+
+class MyComponent extends React.Component
+{
+  render () {
+    return <h2>
+      Another Thing
+    </h2>
+  }
+}
+
+render(
+  <Main name='Josh' />,
+  document.getElementById('app')
+)
